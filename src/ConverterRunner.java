e4a2132945d1e70b084c0aca448652a0059e5ba3
Veronica Joseph <lalalal@gmail.com>
2024-01-09 19:02:47 -0500

    - fixed the placements of the conditionals to check if the base and number are valid

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -5,54 +5,55 @@ class ConverterRunner {
     public static void main(String[] args) {
         System.out.println("Welcome to the Number Converter!");
         System.out.println("--------------------------------");
-        System.out.print("Enter the base of your number (2, 8 or 10): ");
 
         Scanner s = new Scanner(System.in);
-        String choice = s.nextLine();
-        int base = Integer.parseInt(choice);
+        int base = 0;
+        // loop to check if the user entered a valid base
+        boolean chosen = false;
+        while (!chosen) {
+            System.out.print("Enter the base of your number (2, 8 or 10): ");
+            String choice = s.nextLine();
+            int choice2 = Integer.parseInt(choice);
+            if (choice2 == 2 || choice2 == 8 || choice2 == 10) {
+                base += choice2;
+                System.out.println();
+                chosen = true;
+            }
+            else {
+                System.out.println("That's not a valid base!");
+            }
+        }
 
         System.out.print("Enter your number: ");
         String number = s.nextLine();
         int n = Integer.parseInt(number);
-
         s.close();
 
         NumberConverter nc = new NumberConverter(n, base);
         int[] digits = nc.getDigits();
-        System.out.println("\n\nDigit array: " + Arrays.toString(digits));
-        System.out.println("Number: " + nc.displayOriginalNumber());
 
-        boolean invalidBinaryDigit = false;
-        boolean invalidOctalDigit = false;
-        boolean invalidB10Digit = false;
 
-        // checks if the number is valid (need to put in loop)
-        if (base == 2) {
-            for (int i = 0; i < digits.length && !invalidBinaryDigit; i++) {
+        // checks whether the number entered matches with the base chosen
+        boolean validDigits = true;
+        for (int i = 0; i < digits.length && validDigits; i++) {
+            if (base == 2) {
                 if (!(digits[i] == 0 || digits[i] == 1)) {
                     System.out.println("That's not a valid binary number!");
-                    invalidBinaryDigit = true;
+                    validDigits = false;
                 }
             }
-        }
-        else if (base == 8) {
-            for (int i = 0; i < digits.length && !invalidOctalDigit; i++) {
-                if (!(digits[i] == 8 || digits[i] == 9)) {
+            else if (base == 8) {
+                if (!(digits[i] >= 0 && digits[i] <= 7)) {
                     System.out.println("That's not a valid octal number!");
-                    invalidOctalDigit = true;
+                    validDigits = false;
                 }
             }
-        }
-        else if (base == 10) {
-            for (int i = 0; i < digits.length && !invalidB10Digit; i++) {
+            else {
                 if (!Character.isDigit(digits[i])) {
                     System.out.println("That's not a valid base 10 number!");
-                    invalidB10Digit = true;
+                    validDigits = false;
                 }
             }
-        }
-        else {
-            System.out.println("That's not a valid base!");
         }
 
     }
